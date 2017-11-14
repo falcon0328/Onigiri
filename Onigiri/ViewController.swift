dc74eef80854948a52d63b6734c3622ba90a7e0b
FALCON <falcon.x.0328@gmail.com>
2017-11-14 13:48:26 +0900

    Project Onigiri init

---
+++ Onigiri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onigiri
+//
+//  Created by falcon@mac on H29/11/14.
+//  Copyright © 平成29年 NagaoLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
